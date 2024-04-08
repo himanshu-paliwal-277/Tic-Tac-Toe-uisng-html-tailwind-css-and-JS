d680d66d0f040c76a42b8c108417ed4310f450cf
Himanshu Paliwal <hpaliwal364@gmail.com>
2024-04-08 20:44:04 +0530

    Advance level feature is added

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ let congratulations_window = document.getElementById("congratulations_window");
 let showing_which_player_turn = document.getElementById(
   "showing_which_player_turn"
 );
+let easy_level_button = document.getElementById("easy_level_button");
+let advance_level_button = document.getElementById("advance_level_button");
+
 let disable_div = document.getElementById("disable_div");
 let click_sound = new Audio("./Assets/click-button-sound.wav");
 
@@ -18,6 +21,9 @@ let computer_mode = false;
 let already_win = false;
 let plater_1_click_in_computer_mode = "X";
 
+// Game Level:
+let advance_level = false;
+
 buttons.forEach((element) => {
   element.addEventListener("click", () => {
     if (element.innerHTML === "") {
@@ -26,20 +32,20 @@ buttons.forEach((element) => {
         element.innerText = plater_1_click_in_computer_mode;
         plater_1_click_in_computer_mode = "";
         setTimeout(() => {
-            plater_1_click_in_computer_mode = "X";
+          plater_1_click_in_computer_mode = "X";
         }, 400);
         change_player_turn();
         showing_which_player_turn.innerText = `${player_turn}`;
         check_winner();
-        
-        if(already_win === false){
-            // Logic for playing with computer
-            if(showing_which_player_turn.innerText === "O"){
-                computer_move();
-            }
-            setTimeout(() => {
-                check_winner();
-            }, 400);
+
+        if (already_win === false) {
+          // Logic for playing with computer
+          if (showing_which_player_turn.innerText === "O") {
+            computer_move();
+          }
+          setTimeout(() => {
+            check_winner();
+          }, 400);
         }
       } else {
         click_sound.play();
@@ -185,45 +191,271 @@ function winner(player) {
 
 // Logic if player 1 button is clicked
 player_1_button.addEventListener("click", () => {
-start_window.style.height = `0vh`;
-document.querySelector(".start_window_div").classList.add("hidden");
+  start_window.style.height = `0vh`;
+  document.querySelector(".start_window_div").classList.add("hidden");
   computer_mode = true;
 });
 
 // Logic if player 2 button is clicked
 player_2_button.addEventListener("click", () => {
-    start_window.style.height = `0vh`;
-    document.querySelector(".start_window_div").classList.add("hidden");
+  start_window.style.height = `0vh`;
+  document.querySelector(".start_window_div").classList.add("hidden");
+  easy_level_button.classList.add("hidden");
+  advance_level_button.classList.add("hidden");
 });
 
 // Function which automatic play moves (play with computer mode)
 function computer_move() {
-    let move = Math.floor(Math.random() * 9);
-    // console.log("Move = ",move);
-    if(buttons[move].innerText === ""){
-        setTimeout(() => {
-            buttons[move].innerText = "O";
-            player_turn = "X";
-            showing_which_player_turn.innerText = `${player_turn}`;
-        }, 400);
-    }
-    else {
-        let is_atleast_one_empty = false;
-        buttons.forEach(element => {
-            if(element.innerText === ""){
-                is_atleast_one_empty = true;
-            }
-        })
-        if(is_atleast_one_empty){
-            while(buttons[move].innerText !== ""){
-                move = Math.floor(Math.random() * 9);
-            }
-            // console.log("Move regenerate = ",move);
-            setTimeout(() => {
-                buttons[move].innerText = "O";
-                player_turn = "X";
-                showing_which_player_turn.innerText = `${player_turn}`;
-            }, 400);
-        }
+  let move = Math.floor(Math.random() * 9);
+  // console.log("Move = ",move);
+  if (advance_level) {
+    advance_level_move();
+  } else if (buttons[move].innerText === "") {
+    setTimeout(() => {
+      buttons[move].innerText = "O";
+      player_turn = "X";
+      showing_which_player_turn.innerText = `${player_turn}`;
+    }, 400);
+  } else {
+    let is_atleast_one_empty = false;
+    buttons.forEach((element) => {
+      if (element.innerText === "") {
+        is_atleast_one_empty = true;
+      }
+    });
+    if (is_atleast_one_empty) {
+      while (buttons[move].innerText !== "") {
+        move = Math.floor(Math.random() * 9);
+      }
+      // console.log("Move regenerate = ",move);
+      setTimeout(() => {
+        buttons[move].innerText = "O";
+        player_turn = "X";
+        showing_which_player_turn.innerText = `${player_turn}`;
+      }, 400);
     }
   }
+}
+
+// Game levels logic
+// easy level button click logic
+easy_level_button.addEventListener("click", () => {
+  easy_level_button.classList.remove("bg-blue-600");
+  easy_level_button.classList.add("bg-red-600");
+  advance_level_button.classList.add("bg-blue-600");
+  advance_level_button.classList.remove("bg-red-600");
+  advance_level = false;
+  reset();
+});
+
+// advance level button click logic
+advance_level_button.addEventListener("click", () => {
+  advance_level_button.classList.remove("bg-blue-600");
+  advance_level_button.classList.add("bg-red-600");
+  easy_level_button.classList.add("bg-blue-600");
+  easy_level_button.classList.remove("bg-red-600");
+  advance_level = true;
+  reset();
+});
+
+// advance level logic
+function advance_level_move() {
+  let move = Math.floor(Math.random() * 9);
+  let j = 0;
+  for (let i = 0; i < 9; i += 3) {
+    // For Row
+    if (
+      buttons[i].innerText === "X" &&
+      buttons[i + 1].innerText === "X" &&
+      buttons[i + 2].innerText === ""
+    ) {
+      setTimeout(() => {
+        buttons[i + 2].innerText = "O";
+        player_turn = "X";
+        showing_which_player_turn.innerText = `${player_turn}`;
+      }, 400);
+      return;
+    }
+    // For Row
+    else if (
+      buttons[i].innerText === "X" &&
+      buttons[i + 1].innerText === "" &&
+      buttons[i + 2].innerText === "X"
+    ) {
+      setTimeout(() => {
+        buttons[i + 1].innerText = "O";
+        player_turn = "X";
+        showing_which_player_turn.innerText = `${player_turn}`;
+      }, 400);
+      return;
+    }
+    // For Row
+    else if (
+      buttons[i].innerText === "" &&
+      buttons[i + 1].innerText === "X" &&
+      buttons[i + 2].innerText === "X"
+    ) {
+      setTimeout(() => {
+        buttons[i].innerText = "O";
+        player_turn = "X";
+        showing_which_player_turn.innerText = `${player_turn}`;
+      }, 400);
+      return;
+    }
+    // For Column
+    else if (
+      buttons[j].innerText === "X" &&
+      buttons[j + 3].innerText === "X" &&
+      buttons[j + 6].innerText === ""
+    ) {
+      setTimeout(() => {
+        buttons[j + 6].innerText = "O";
+        player_turn = "X";
+        showing_which_player_turn.innerText = `${player_turn}`;
+      }, 400);
+      return;
+    }
+    // For Column
+    else if (
+      buttons[j].innerText === "X" &&
+      buttons[j + 3].innerText === "" &&
+      buttons[j + 6].innerText === "X"
+    ) {
+      setTimeout(() => {
+        buttons[j + 3].innerText = "O";
+        player_turn = "X";
+        showing_which_player_turn.innerText = `${player_turn}`;
+      }, 400);
+      return;
+    }
+    // For Column
+    else if (
+      buttons[j].innerText === "" &&
+      buttons[j + 3].innerText === "X" &&
+      buttons[j + 6].innerText === "X"
+    ) {
+      setTimeout(() => {
+        buttons[j].innerText = "O";
+        player_turn = "X";
+        showing_which_player_turn.innerText = `${player_turn}`;
+      }, 400);
+      return;
+    }
+    // For Diagonal 1
+    else if (i === 0) {
+      if (
+        buttons[i].innerText === "X" &&
+        buttons[i + 4].innerText === "X" &&
+        buttons[i + 8].innerText === ""
+      ) {
+        setTimeout(() => {
+          buttons[i + 8].innerText = "O";
+          player_turn = "X";
+          showing_which_player_turn.innerText = `${player_turn}`;
+        }, 400);
+        return;
+      }
+    }
+    // For Diagonal 1
+    else if (i === 0) {
+      if (
+        buttons[i].innerText === "X" &&
+        buttons[i + 4].innerText === "" &&
+        buttons[i + 8].innerText === "X"
+      ) {
+        setTimeout(() => {
+          buttons[i + 4].innerText = "O";
+          player_turn = "X";
+          showing_which_player_turn.innerText = `${player_turn}`;
+        }, 400);
+        return;
+      }
+    }
+    // For Diagonal 1
+    else if (i === 0) {
+      if (
+        buttons[i].innerText === "" &&
+        buttons[i + 4].innerText === "X" &&
+        buttons[i + 8].innerText === "X"
+      ) {
+        setTimeout(() => {
+          buttons[i].innerText = "O";
+          player_turn = "X";
+          showing_which_player_turn.innerText = `${player_turn}`;
+        }, 400);
+        return;
+      }
+    }
+    // For Diagonal 2
+    else if (j === 2) {
+      if (
+        buttons[j].innerText === "" &&
+        buttons[j + 2].innerText === "X" &&
+        buttons[j + 4].innerText === "X"
+      ) {
+        setTimeout(() => {
+          buttons[j].innerText = "O";
+          player_turn = "X";
+          showing_which_player_turn.innerText = `${player_turn}`;
+        }, 400);
+        return;
+      }
+    }
+    // For Diagonal 2
+    else if (j === 2) {
+      if (
+        buttons[j].innerText === "X" &&
+        buttons[j + 2].innerText === "X" &&
+        buttons[j + 4].innerText === ""
+      ) {
+        setTimeout(() => {
+          buttons[j + 4].innerText = "O";
+          player_turn = "X";
+          showing_which_player_turn.innerText = `${player_turn}`;
+        }, 400);
+        return;
+      }
+    }
+    // For Diagonal 2
+    else if (j === 2) {
+      if (
+        buttons[j].innerText === "X" &&
+        buttons[j + 2].innerText === "" &&
+        buttons[j + 4].innerText === "X"
+      ) {
+        setTimeout(() => {
+          buttons[j + 2].innerText = "O";
+          player_turn = "X";
+          showing_which_player_turn.innerText = `${player_turn}`;
+        }, 400);
+        return;
+      }
+    }
+    j++;
+  }
+  if (buttons[move].innerText === "") {
+    setTimeout(() => {
+      buttons[move].innerText = "O";
+      player_turn = "X";
+      showing_which_player_turn.innerText = `${player_turn}`;
+    }, 400);
+  } else {
+    let is_atleast_one_empty = false;
+    buttons.forEach((element) => {
+      if (element.innerText === "") {
+        is_atleast_one_empty = true;
+      }
+    });
+    if (is_atleast_one_empty) {
+      while (buttons[move].innerText !== "") {
+        move = Math.floor(Math.random() * 9);
+      }
+      // console.log("Move regenerate = ",move);
+      setTimeout(() => {
+        buttons[move].innerText = "O";
+        player_turn = "X";
+        showing_which_player_turn.innerText = `${player_turn}`;
+      }, 400);
+    }
+  }
+}
